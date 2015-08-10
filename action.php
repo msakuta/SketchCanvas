cb1032d0e0d731f165e067ffd612374d29726931
msakuta <masahiro.sakuta@gmail.com>
2015-08-11 06:02:55 +0900

    Add a new button to add a new figure in edit mode
    It now works very much like edittable plugin.  If a figure is opened
    with edit button directly, pressing save button will bring the user
    back to page view.  If one is created with the new figure button in
    the toolbar in edit page, pressing save button will bring the user
    back to previous edit page.

--- action.php
+++ action.php
@@ -22,6 +22,9 @@ class action_plugin_skcanvas extends DokuWiki_Action_Plugin {
 
        $controller->register_hook('HTML_SECEDIT_BUTTON', 'BEFORE', $this, 'editButton');
        $controller->register_hook('HTML_EDIT_FORMSELECTION', 'BEFORE', $this, 'editForm');
+       $controller->register_hook('ACTION_ACT_PREPROCESS', 'BEFORE', $this, 'handle_newfigure');
+
+       $controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'toolbarDefine');
     }
 
     /**
@@ -86,7 +89,8 @@ EOT
 
         $escText = '"' . str_replace(array("\r", "\n"), array('\r', '\n'), addslashes($TEXT)) . '"';
 
-        echo <<<EOT
+        $form =& $event->data['form'];
+        $canvasText = <<<EOT
 <canvas id="editcanvas"></canvas>
 <script type="text/javascript"><!--
 (function(){
@@ -98,9 +102,84 @@ EOT
     }
 })();
 --></script>
+<textarea name="wikitext" id="wiki__text" class="edit" cols="80" rows="10">$TEXT</textarea>
 EOT;
-        $attr = array();
-        if(!$event->data['wr']) $attr['readonly'] = 'readonly';
-        $event->data['form']->addElement(form_makeWikiText($TEXT, $attr));
+        $form->addElement($canvasText);
+
+        // Pass wikitext through POSTs for previewing and saving
+        if(isset($_POST['editfigure__new'])) {
+            foreach($_POST['editfigure__new'] as $k => $v) {
+                $form->addHidden("editfigure__new[$k]", $v);
+            }
+        }
+    }
+
+    /**
+     * Add a toolbar button to add a new figure
+     *
+     * @param Doku_Event $event
+     */
+    public function toolbarDefine(Doku_Event $event, $param){
+        $event->data[] = array(
+            'type' => 'NewFigure',
+            'title' => 'New Figure',
+            'icon' => '../../plugins/' . $this->getPluginName() . '/images/figure.png',
+            'block' => true);
+    }
+
+    /**
+     * Handle the click on the new figure button in the toolbar
+     *
+     * @param Doku_Event $event
+     */
+    function handle_newfigure($event) {
+        global $INPUT;
+        global $TEXT;
+        global $ACT;
+
+        if(!$INPUT->post->has('editfigure__new')) return;
+
+        /*
+         * $fields['pre']  has all data before the selection when the "Insert table" button was clicked
+         * $fields['text'] has all data inside the selection when the "Insert table" button was clicked
+         * $fields['suf']  has all data after the selection when the "Insert table" button was clicked
+         * $TEXT has the table created by the editor (from action_plugin_edittable_editor::handle_table_post())
+         */
+        $fields = $INPUT->post->arr('editfigure__new');
+
+        // clean the fields (undos formText()) and update the post and request arrays
+        $fields['pre'] = cleanText($fields['pre']);
+        $fields['text'] = cleanText($fields['text']);
+        $fields['suf'] = cleanText($fields['suf']);
+        $INPUT->post->set('editfigure__new', $fields);
+
+
+        $ACT = act_clean($ACT);
+        switch($ACT){
+            case 'preview':
+                // preview view of a table edit
+                $INPUT->post->set('target', 'plugin_skcanvas');
+                break;
+            case 'edit':
+                // edit view of a table (first edit)
+                $INPUT->post->set('target', 'plugin_skcanvas');
+                $TEXT = "";
+                foreach(explode("\n", $fields['text']) as $line) {
+                    $TEXT .= "$line\n";
+                }
+                break;
+            case 'draftdel':
+                // not sure if/how this would happen, we restore all data and hand over to section edit
+                $INPUT->post->set('target', 'section');
+                $TEXT = $fields['pre'].$fields['text'].$fields['suf'];
+                $ACT  = 'edit';
+                break;
+            case 'save':
+                // return to edit page
+                $INPUT->post->set('target', 'section');
+                $TEXT = $fields['pre']."<skcanvas>\n".$TEXT."</skcanvas>".$fields['suf'];
+                $ACT  = 'edit';
+                break;
+        }
     }
 }
