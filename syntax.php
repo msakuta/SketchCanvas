84edfb576e5a9accfc5166b57c0bc4412a25522b
Andreas Gohr <andi@splitbrain.org>
2016-01-24 13:29:19 +0100

    Adjust method signatures to match parent

--- syntax.php
+++ syntax.php
@@ -31,7 +31,7 @@ class syntax_plugin_sketchcanvas extends DokuWiki_Syntax_Plugin {
     /**
      * Handle the match
      */
-    function handle($match, $state, $pos, &$handler){
+    function handle($match, $state, $pos, Doku_Handler $handler){
         switch ($state) {
           case DOKU_LEXER_ENTER :
                 return array($state, array(true, $this->generator, 'bytepos_start' => $pos + strlen($match)));
@@ -45,7 +45,7 @@ class syntax_plugin_sketchcanvas extends DokuWiki_Syntax_Plugin {
     /**
      * Create output
      */
-    function render($mode, &$renderer, $data) {
+    function render($mode, Doku_Renderer $renderer, $data) {
         if($mode == 'xhtml'){
             list($state, $match) = $data;
             switch ($state) {
