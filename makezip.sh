f97fcd020e9bbdff9347856b6997af969654438e
msakuta <masahiro.sakuta@gmail.com>
2024-01-08 00:10:47 +0900

    Add CanvasElement.php to zip file to download

--- makezip.sh
+++ makezip.sh
@@ -1,1 +1,1 @@
-zip -u -r sketchcanvas.zip plugin.info.txt action.php script.js style.less syntax.php README.md phplib script media images
+zip -u -r sketchcanvas.zip plugin.info.txt action.php CanvasElement.php script.js style.less syntax.php README.md phplib script media images
